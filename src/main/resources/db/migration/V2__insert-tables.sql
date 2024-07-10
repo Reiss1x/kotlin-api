dafa44c66bc5065b516896623a9c96a3743136dc
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-07-10 16:57:35 -0300

    fix: now using flyway for table creation and insertion

---
+++ src/main/resources/db/migration/V2__insert-tables.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, prices, description) VALUES ('Banana', 2.99, 'Fruta');
+INSERT INTO product (name, prices, description) VALUES ('Maçã', 1.49, 'Fruta comum');
+INSERT INTO product (name, prices, description) VALUES ('Abacaxi', 5.99, 'Fruta tropical');
+INSERT INTO product (name, prices, description) VALUES ('Morango', 3.79, 'Fruta vermelha e doce');
+INSERT INTO product (name, prices, description) VALUES ('Uva', 4.99, 'Fruta pequena e doce');
