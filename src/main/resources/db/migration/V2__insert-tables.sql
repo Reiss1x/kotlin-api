c1ebaa1008fedd9d0eeb2f48456d9c516c0c26ee
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-07-12 00:10:31 -0300

    feat: added docker-compose project inicialization

--- src/main/resources/db/migration/V2__insert-tables.sql
+++ src/main/resources/db/migration/V2__insert-tables.sql
@@ -7,7 +7,6 @@ INSERT INTO product (name, prices) VALUES ('Maçã', 1.49);
 INSERT INTO product (name, prices) VALUES ('Abacaxi', 5.99);
 
 INSERT INTO product_category (product_id, category_id) VALUES (1, 1);
-INSERT INTO product_category (product_id, category_id) VALUES (2, 1);
 INSERT INTO product_category (product_id, category_id) VALUES (1, 2);
 INSERT INTO product_category (product_id, category_id) VALUES (2, 2);
 INSERT INTO product_category (product_id, category_id) VALUES (3, 3);
--- src/main/resources/import-dev.sql
+++
@@ -1,4 +0,0 @@
-insert into product(prices, name, description) VALUES (2.99, 'Banana', 'Fruta');
-INSERT INTO product(prices, name, description) VALUES (1.49, 'Maçã', 'Fruta comum');
-INSERT INTO product(prices, name, description) VALUES (5.99, 'Abacaxi', 'Fruta tropical');
-INSERT INTO product(prices, name, description) VALUES (3.79, 'Morango', 'Fruta vermelha e doce');
